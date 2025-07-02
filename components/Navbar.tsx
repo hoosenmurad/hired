25adbc1342eeb5524208b8e3c088c512fe5868b5
MuradHoosen <hoosenmurad@icloud.com>
2025-07-02 18:42:16 +0200

    Refactor layout and Navbar components for improved structure
    - Removed direct navigation links from layout.tsx, simplifying the component.
    - Updated Navbar component to include the application name "HiredAI" for better branding.
    - Streamlined the layout structure to focus on child components.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,8 @@ const Navbar = () => {
     <nav className="navbar">
       <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
-          <Image src="/logo.svg" alt="logo" width={46} height={44} />
+          <Image src="/logo.svg" alt="logo" width={46} height={44} />{" "}
+          <h2 className="text-primary-100">HiredAI</h2>
         </div>
       </Link>
       <div className="flex items-center gap-8">
